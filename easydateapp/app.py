c34181c248374a55af40ce1eaf22a7ab0f57ef3c
MatthieuALLIER <81558595+MatthieuALLIER@users.noreply.github.com>
2022-10-12 16:50:35 +0200

    Add files via upload

--- easydateapp/app.py
+++ easydateapp/app.py
@@ -7,66 +7,91 @@ app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], title='EasyDat
 
 df = pd.read_csv("trainClean.csv")
 
-#CSS placement
-SIDEBAR_STYLE = {
-    "position": "fixed",
-    "top": 0,
-    "left": 0,
-    "bottom": 0,
-    "width": "16rem",
-    "padding": "2rem 1rem",
-    "background-color": "#f8f9fa",
-}
-CONTENT_STYLE = {
-    "margin-left": "18rem",
-    "margin-right": "2rem",
-    "padding": "2rem 1rem",
-}
-
-fig = px.scatter(df, x="age", y="income", color="gender")
 #SideBar
-SideBar = html.Div(
-    [
-        html.H2("Menu", className="display-4"),
-        html.Hr(),
-        dbc.Nav(
-            [
-                dbc.NavLink("Accueil", href="/", active="exact"),
-                dbc.NavLink("Statistique", href="/Statistique", active="exact"),
-                dbc.NavLink("Modélisation", href="/Modelisation", active="exact"),
-                dbc.NavLink("Prédiction", href="/Prediction", active="exact"),                
-            ],
-            vertical=True,
-            pills=True,
-        ),
-    ],
-    style=SIDEBAR_STYLE
-)
+TABPANEL = dbc.Container([
+    html.H1("Menu"),
+    html.Hr(),
+    dbc.Tabs(
+        [
+            dbc.Tab(label="Accueil", tab_id="Accueil"),
+            dbc.Tab(label="Statistique", tab_id="Statistique"),
+            dbc.Tab(label="Modélisation", tab_id="Modelisation"),
+            dbc.Tab(label="Prédiction", tab_id="Prediction")             
+        ],
+        id="tabPanel",
+        active_tab="Accueil",
+    )
+])
+        
 #Contenu
-PageContent = html.Div(id="page-content", style=CONTENT_STYLE)
+PageContent = dbc.Container([
+
+    #Accueil
+    html.Div(id="Accueil-tab", children=
+        html.Div(
+            html.P("Accueil")
+        )
+    ),
+
+    #Page Statistique
+    html.Div(id="Statistique-tab", children=[
+        html.Div([
+            dcc.Dropdown(id="xInput", options=[{"label":name,"value":name} for name in df.columns], value="age", className="app-DivFilter--Select_value"),
+            dcc.Dropdown(id="yInput", options=[{"label":name,"value":name} for name in df.columns], value="income", className="app-DivFilter--Select_value"),
+            dcc.Dropdown(id="colorInput", options=[{"label":name,"value":name} for name in df.columns], value="gender", className="app-DivFilter--Select_value")], 
+            className="DivFilter"
+        ),
+        dcc.Graph(id="GraphStat_1")
+    ], className="DivTab"),
+
+    #Page Modélisation
+    html.Div(id="Modelisation-tab", children=
+        html.Div(
+            html.P("Modelisation")
+        )
+    ),
+
+    #Page prédiction
+    html.Div(id="Prediction-tab", children=
+        html.Div(
+            html.P("Prediction")
+        )
+    )
+])
+
 
 #Apparence
-app.layout = html.Div([dcc.Location(id="url"), SideBar, PageContent])
+app.layout = html.Div([TABPANEL, PageContent])
 
-@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
-def render_page_content(pathname):
-    if pathname == "/":
-        return html.P("Accueil de Légende")
-    elif pathname == "/Statistique":
-        return dcc.Graph(id='example-graph-2', figure=fig)
-    elif pathname == "/Modelisation":
-        return html.P("Ici on modélise")
-    elif pathname == "/Prediction":
-        return html.P("Vla la prediction en mode API rest")
-    # If the user tries to reach a different page, return a 404 message
-    return html.Div(
-        [
-            html.H1("404: Not found", className="text-danger"),
-            html.Hr(),
-            html.P(f"The pathname {pathname} was not recognised..."),
-        ],
-        className="p-3 bg-light rounded-3",
-    )
+@app.callback([Output("Accueil-tab", "style"), Output("Statistique-tab", "style"), Output("Modelisation-tab", "style"), Output("Prediction-tab", "style")],
+              [Input("tabPanel","active_tab")])
+def render_tab_content(active_tab):
+    on = {"display":"block"}
+    off = {"display":"none"}
+    if active_tab is not None:
+        if active_tab == "Accueil":
+            return on, off, off, off
+        elif active_tab == "Statistique":
+            return off, on, off, off
+        elif active_tab == "Modelisation":
+            return off, off, on, off
+        elif active_tab == "Prediction":
+            return off, off, off, on
+    return "No tab selected"
+
+
+@app.callback(Output('GraphStat_1', 'figure'),
+    [Input('xInput', 'value'), Input('yInput', 'value'), Input('colorInput', 'value')])
+def update_graph(xInput, yInput, colorInput):
+    
+    dfg = df.groupby(by=[xInput,colorInput])[yInput].mean().reset_index()
+    dfg[colorInput] = dfg[colorInput].astype(str)
+    fig = px.bar(dfg, x=xInput,
+                     y=yInput,
+                     color=colorInput,
+                     barmode="group")
+    return fig
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
