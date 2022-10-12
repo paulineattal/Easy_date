8160334e92240dfa1d7916a47534ab6f6eac70c7
Pauline ATTAL <pauline.attal8@gmail.com>
2022-10-12 18:21:06 +0200

    change for heroku

--- app.py
+++ app.py
@@ -5,11 +5,8 @@ import pandas as pd
 import gunicorn 
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], title='EasyDate Michael Scott Team')
-# Reference the underlying flask app (Used by gunicorn webserver in Heroku production deployment)
 server = app.server 
 
-# Enable Whitenoise for serving static files from Heroku (the /static folder is seen as root by Heroku) 
-server.wsgi_app = WhiteNoise(server.wsgi_app, root='static/') 
 df = pd.read_csv("trainClean.csv")
 
 #SideBar
